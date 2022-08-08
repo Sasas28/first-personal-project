978778280d8e2975978220940e2018350f39ed00
Ajaster <ajasterzamora28@gmail.com>
2022-08-08 20:41:11 +0800

    Completed header and hero section UI

--- site/assets/scripts/App.js
+++ site/assets/scripts/App.js
@@ -2,4 +2,4 @@ import '../styles/style.css' //App js file will render the style onto index.html
 
 if (module.hot) {
     module.hot.accept()
-}
+} //if true hot module will be enable and will automatically render changes.

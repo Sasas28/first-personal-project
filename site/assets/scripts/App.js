f52ba0a8e515f7c133fe657c45c7dbb60b28b542
Ajaster <ajasterzamora28@gmail.com>
2022-08-08 15:09:15 +0800

    Completed automated refresh/reload configuration

--- site/assets/scripts/App.js
+++ site/assets/scripts/App.js
@@ -1,2 +1,5 @@
 import '../styles/style.css' //App js file will render the style onto index.html file.
-alert("hello world")
+
+if (module.hot) {
+    module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const postCSSPlugins = [
+    require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -11,15 +12,24 @@ module.exports = {
         filename: 'bundled.js', //set your desired js file name.
         path: path.resolve(__dirname, 'site') // set where you want to put your created js file.
     },
+    devServer: {
+        before: function(site, server) {
+            server._watch('./site/**/*.html') //automatic reload everytime html file save changes.
+        },
+        contentBase: path.join(__dirname, 'site'), //webpack is pointing towards this directory.
+        hot: true, //automatic reload everytime css and js file save changes.
+        port: 3000, //localhost portal.
+        host: '0.0.0.0' //to have access on other device.
+    },
     mode: 'development', //set your environment as development.
-    watch: true, //webpack will continue to bundle that App.js file.
+  //watch: true, webpack will continue to bundle that App.js file.
     module: {
         rules: [
             {
                 test:/\.css$/i, //webpack will test this file.
                 use: [
                         'style-loader', 
-                        'css-loader', 
+                        'css-loader?url=false', //disable 'css-loader' in order to manage our image file manually.
                         {
                             loader: 'postcss-loader', 
                             options: {

3b3ffcdc036c02225b8bb3ea15194b1449d2ff68
Ajaster <ajasterzamora28@gmail.com>
2022-08-08 14:01:02 +0800

    Done postcss configuration.

--- site/assets/scripts/App.js
+++ site/assets/scripts/App.js
@@ -1,1 +1,2 @@
+import '../styles/style.css' //App js file will render the style onto index.html file.
 alert("hello world")
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
 
 module.exports = {
     entry: './site/assets/scripts/App.js', //webpack will watch this file.
@@ -7,5 +12,22 @@ module.exports = {
         path: path.resolve(__dirname, 'site') // set where you want to put your created js file.
     },
     mode: 'development', //set your environment as development.
-    watch: true //webpack will continue to bundle that App.js file.
+    watch: true, //webpack will continue to bundle that App.js file.
+    module: {
+        rules: [
+            {
+                test:/\.css$/i, //webpack will test this file.
+                use: [
+                        'style-loader', 
+                        'css-loader', 
+                        {
+                            loader: 'postcss-loader', 
+                            options: {
+                                postcssOptions: {plugins: postCSSPlugins}
+                            }
+                        }
+                    ] //webpack will use this packages to test the file.
+            }
+        ]
+    }
 }

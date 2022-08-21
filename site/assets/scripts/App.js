f8f79104643947c7996c06f27994c3e828eba049
Ajaster <ajasterzamora28@gmail.com>
2022-08-21 11:53:22 +0800

    Updating navigation on mobile view

--- site/assets/scripts/App.js
+++ site/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import '../styles/style.css' //App js file will render the style onto index.html file.
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu()
 
 if (module.hot) {
     module.hot.accept()
-} //if true hot module will be enable and will automatically render changes.
+} //if true, hot module will be enable and will automatically render changes.
